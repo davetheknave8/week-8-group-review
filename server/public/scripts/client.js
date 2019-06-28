5aab7de01d5b333d03f28b209fd6235ed6084f59
David Reeves <davidsreeves1@gmail.com>
2019-06-28 09:37:02 -0500

    ready to start server work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,4 +10,14 @@ function onReady(){
 function sendMessage(){
     event.preventDefault();
     console.log('Send Message');
+    // get the user input
+    // prepare object to send to server
+    let objectToSend = {
+        userName: $('#usernameIn').val(),
+        messageText: $('#messageTextIn').val()
+    }
+    console.log('sending:', objectToSend);
+    //send to server
+    // clear just message text
+    $('#messageTextIn').val('');
 }
